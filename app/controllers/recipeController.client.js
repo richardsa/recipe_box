8ee0522c7d8a064b081414d00f76b9fe49f3daa1
Richard Saldivar <rsaldiva@gmail.com>
2016-09-30 01:19:45 +0000

    initial form success

--- app/controllers/recipeController.client.js
+++ app/controllers/recipeController.client.js
@@ -2,88 +2,95 @@
 
 (function() {
 
-$(document).ready(function() {
+
    
-  var titleValidators = {
-            row: '.col-xs-4',   // The title is placed inside a <div class="col-xs-4"> element
-            validators: {
-                notEmpty: {
-                    message: 'The title is required'
-                }
+ $(document).ready(function() {
+      var titleValidators = {
+          row: '.col-xs-4', // The title is placed inside a <div class="col-xs-4"> element
+          validators: {
+            notEmpty: {
+              message: 'The title is required'
             }
+          }
         },
         isbnValidators = {
-            row: '.col-xs-4',
-            validators: {
-                notEmpty: {
-                    message: 'The ISBN is required'
-                },
-                isbn: {
-                    message: 'The ISBN is not valid'
-                }
+          row: '.col-xs-4',
+          validators: {
+            notEmpty: {
+              message: 'The ISBN is required'
+            },
+            isbn: {
+              message: 'The ISBN is not valid'
             }
+          }
         },
         priceValidators = {
-            row: '.col-xs-2',
-            validators: {
-                notEmpty: {
-                    message: 'The price is required'
-                },
-                numeric: {
-                    message: 'The price must be a numeric number'
-                }
+          row: '.col-xs-2',
+          validators: {
+            notEmpty: {
+              message: 'The price is required'
+            },
+            numeric: {
+              message: 'The price must be a numeric number'
             }
+          }
         },
         bookIndex = 0;
 
-    $('#uploadForm')
+      $('#uploadForm')
         .formValidation({
-            framework: 'bootstrap',
-            icon: {
-                valid: 'glyphicon glyphicon-ok',
-                invalid: 'glyphicon glyphicon-remove',
-                validating: 'glyphicon glyphicon-refresh'
-            },
-            fields: {
-                'book[0].title': titleValidators,
-                'book[0].isbn': isbnValidators,
-                'book[0].price': priceValidators
-            }
+          framework: 'bootstrap',
+          icon: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+          },
+          fields: {
+            'book[0].title': titleValidators,
+            'book[0].isbn': isbnValidators,
+            'book[0].price': priceValidators
+          }
         })
 
       // Add button click handler
       .on('click', '.addButton', function() {
-          bookIndex++;
-          var $template = $('#bookTemplate'),
-              $clone    = $template
-                              .clone()
-                              .removeClass('hide')
-                              .removeAttr('id')
-                              .attr('data-book-index', bookIndex)
-                              .insertBefore($template);
+        bookIndex++;
+        var $template = $('#ingredientTemplate'),
+          $clone = $template
+          .clone()
+          .removeClass('hide')
+          .removeAttr('id')
+          .attr('data-book-index', bookIndex)
+          .insertBefore($template);
 
-          // Update the name attributes
-          $clone
-              .find('[name="title"]').attr('name', 'book[' + bookIndex + '].title').end()
-              .find('[name="isbn"]').attr('name', 'book[' + bookIndex + '].isbn').end()
-              .find('[name="price"]').attr('name', 'book[' + bookIndex + '].price').end();
-    
+        // Update the name attributes
+        $clone
+          .find('[name="title"]').attr('name', 'book[' + bookIndex + '].title').end()
+          .find('[name="isbn"]').attr('name', 'book[' + bookIndex + '].isbn').end()
+          .find('[name="price"]').attr('name', 'book[' + bookIndex + '].price').end();
+
+        // Add new fields
+        // Note that we also pass the validator rules for new field as the third parameter
+        $('#uploadForm')
+          .formValidation('addField', 'book[' + bookIndex + '].title', titleValidators)
+          .formValidation('addField', 'book[' + bookIndex + '].isbn', isbnValidators)
+          .formValidation('addField', 'book[' + bookIndex + '].price', priceValidators);
       })
-      
-        // Remove button click handler
-        .on('click', '.removeButton', function() {
-            var $row  = $(this).parents('.form-group'),
-                index = $row.attr('data-book-index');
 
-            // Remove fields
-            $('#uploadForm')
-                .formValidation('removeField', $row.find('[name="book[' + index + '].title"]'))
-                .formValidation('removeField', $row.find('[name="book[' + index + '].isbn"]'))
-                .formValidation('removeField', $row.find('[name="book[' + index + '].price"]'));
+      // Remove button click handler
+      .on('click', '.removeButton', function() {
+        var $row = $(this).parents('.form-group'),
+          index = $row.attr('data-book-index');
 
-            // Remove element containing the fields
-            $row.remove();
-        });
-});
+        // Remove fields
+        $('#uploadForm')
+          .formValidation('removeField', $row.find('[name="book[' + index + '].title"]'))
+          .formValidation('removeField', $row.find('[name="book[' + index + '].isbn"]'))
+          .formValidation('removeField', $row.find('[name="book[' + index + '].price"]'));
+
+        // Remove element containing the fields
+        $row.remove();
+      });
+    });
   
 })();
